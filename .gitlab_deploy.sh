095fe00e7a310c77665e232d5561feea0a50dd36
Niharika Singh <nikky.123.singh@gmail.com>
2020-11-18 20:48:52 +0530

    changed gitlab-deploy.sh file

--- .gitlab_deploy.sh
+++ .gitlab_deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-
+#include commands
 #ssh ubuntu@13.233.131.178  "cd /var/www/html && git init && git config user.name "niharika-singh" && echo "test" && git pull https://$user_name:$password@gitlab.com/niharika-singh/test  master"
-"cd /var/www/html && git init && git config user.name "niharika-singh" && echo "test" && git pull https://$user_name:$password@gitlab.com/niharika-singh/test  master"
+"cd /var/www/html && git init && git config user.name "niharika-singh" && echo "test" && git pull https://$user_name:$password@gitlab.com/niharika-singh/testing1  master"
