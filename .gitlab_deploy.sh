c4a451911f10413b0c38271ed40e3b93d4127523
Niharika Singh <nikky.123.singh@gmail.com>
2021-02-07 17:59:47 +0530

    deleted files

--- .gitlab_deploy.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-#include commands.
-#ssh ubuntu@13.233.131.178  "cd /var/www/html && git init && git config user.name "niharika-singh" && echo "test" && git pull https://$user_name:$password@gitlab.com/niharika-singh/test  master"
-ssh ubuntu@65.0.92.62 "cd /var/www/html && git init && git config user.name "niharika-singh" && echo "test" && git pull https://$user_name:$password@gitlab.com/niharika-singh/testing1  master"
